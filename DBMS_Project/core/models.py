7f4de1fcc423632674837cb5ba88f499ce740d7d
Ritwik Gupta <ritwikg.0501@gmail.com>
2025-04-08 15:43:36 +0530

    Initial Setup

---
+++ DBMS_Project/core/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+class Admin(models.Model):
+    ...
+
+
+class Company(models.Model):
+    ...
+
+
+class Placement_Record(models.Model):
+    ...
+
+
+class Job(models.Model):
+    ...
+
+
+class Application(models.Model):
+    ...
+
+
+class Student(models.Model):
+    ...
+
+
+class Training_Enrollment(models.Model):
+    ...
+
+
+class Training_Program(models.Model):
+    ...
+
+
+class Trainer(models.Model):
+    ...
+
+
+class Feedback(models.Model):
+    ...
