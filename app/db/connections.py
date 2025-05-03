17014723ace8bd3f5642bc8cc7c714c6eaa4b07c
Ritwik Gupta <ritwikg.0501@gmail.com>
2025-05-03 08:18:04 +0530

    initialized database

--- app/db/connections.py
+++ app/db/connections.py
@@ -1,10 +1,15 @@
 import os
-from dotenv import load_dotenv
 import psycopg
 
-load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+db_user = os.getenv("DATABASE_USER")
+db_password = os.getenv("DATABASE_PASSWORD")
+db_host = os.getenv("DATABASE_HOST")
+db_port = os.getenv("DATABASE_PORT")
+db_name = os.getenv("DATABASE_NAME")
+
+
+DATABASE_URL = os.getenv(f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
 
 def get_db_connection():
     return psycopg.connect(DATABASE_URL)
